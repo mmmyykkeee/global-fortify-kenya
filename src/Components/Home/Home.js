26436b08671af29242f165cbf2a2294bdcda4563
mmmyykkeee <0mmyykkee@gmail.com>
2023-03-10 12:28:01 +0300

    Animated the welcome words

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -14,7 +14,7 @@ function Home() {
         src="https://www.charings.com/wp-content/uploads/2020/03/banner-shape-05.png"
         alt=""
       />
-     
+
       <img
         className="animate3"
         src="      https://www.charings.com/wp-content/uploads/2020/03/banner-shape-04.png
@@ -24,7 +24,9 @@ function Home() {
       <div className="container">
         <div className="row">
           <div className="col-lg-5">
-            <h1>Get Insurance for almost anything you need</h1>
+            <h1 className="animate__animated animate__slideInLeft animate__delay-0.2s">
+              Get Insurance for almost anything you need
+            </h1>
           </div>
         </div>
       </div>
